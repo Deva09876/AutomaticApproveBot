44bedafb96b33377111c7d13c6034174897b2ef9
Deva09876 <125013272+Deva09876@users.noreply.github.com>
2023-11-05 09:34:53 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,10 +4,9 @@ from dotenv import load_dotenv
 
 # Fill These Variables
 
-START_IMG = getenv("START_IMG" "https://telegra.ph/file/933eea77aca96ed3460b8.png")
 API_ID = int(getenv("API_ID"))
 API_HASH = getenv("API_HASH")
 BOT_TOKEN = getenv("BOT_TOKEN")
-SESSION = getenv("SESSION")
+CHANNEL = getenv("CHANNEL", "Dadeyebotz")
 OWNER_ID = int(getenv("OWNER_ID", 5381777131))
 MONGO = getenv("MONGO")
